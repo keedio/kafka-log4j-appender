998e4d261e6b2eec3ef4dab60441ae1520704629
Luca Rosellini <luca.rosellini@gmail.com>
2017-06-19 14:28:17 +0200

    delayed spark context initialization

--- src/main/java/org/keedio/kafka/log4jappender/SparkContextAwareKafkaLog4jAppender.java
+++ src/main/java/org/keedio/kafka/log4jappender/SparkContextAwareKafkaLog4jAppender.java
@@ -1,5 +1,6 @@
 package org.keedio.kafka.log4jappender;
 
+import org.apache.log4j.Logger;
 import org.apache.log4j.helpers.LogLog;
 import org.apache.log4j.spi.LoggingEvent;
 import org.apache.spark.SparkContext;
@@ -12,16 +13,20 @@ import java.util.Map;
 /**
  * Created by luca on 12/6/17.
  */
-public class SparkContextAwareKafkaLog4jAppender extends KafkaLog4jAppender{
-  
+public class SparkContextAwareKafkaLog4jAppender extends KafkaLog4jAppender {
+
   protected JavaSparkContext sparkContext = null;
 
   @Override
   public void activateOptions() {
     super.activateOptions();
-    
+
+    initSparkContext();
+  }
+
+  private void initSparkContext() {
     Option<SparkContext> optSparkContext = SparkContext$.MODULE$.getActive();
-    
+
     if (optSparkContext.isDefined()) {
       LogLog.warn("Spark context found");
       sparkContext = JavaSparkContext.fromSparkContext(optSparkContext.get());
@@ -33,14 +38,15 @@ public class SparkContextAwareKafkaLog4jAppender extends KafkaLog4jAppender{
   @Override
   protected Map<String, Object> generateEventMetadata(LoggingEvent event) {
     Map<String, Object> metadata = super.generateEventMetadata(event);
-    
-    if (sparkContext != null){
-      String appId = sparkContext.getConf().getAppId();
-      String appName = sparkContext.appName();
-      
-      metadata.put("sparkAppId", appId);
-      metadata.put("sparkAppName", appName);
+
+    if (sparkContext == null) {
+      initSparkContext();
     }
+    String appId = sparkContext.getConf().getAppId();
+    String appName = sparkContext.appName();
+
+    metadata.put("sparkAppId", appId);
+    metadata.put("sparkAppName", appName);
     
     return metadata;
   }
